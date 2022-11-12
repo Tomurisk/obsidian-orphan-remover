71cd480462fa04945285e8bc52b5831d9e943340
Tomurisk <54245284+Tomurisk@users.noreply.github.com>
2022-11-13 00:27:32 +0200

    Changed some formatting so it's more readable

--- delete_orphaned.cpp
+++ delete_orphaned.cpp
@@ -21,84 +21,81 @@ int main()
 	system("dir /b Prisegtukai > cleanup\\attachments.txt");
 
 	vector<string> Directories, Files, Attachments;
+		ifstream GetDirectories;
+			GetDirectories.open("cleanup\\directories.txt");
 
-	ifstream GetDirectories;
-	GetDirectories.open("cleanup\\directories.txt");
+			while (!GetDirectories.eof())
+			{
+				string Directory;
+				getline(GetDirectories, Directory);
+				Directories.push_back(Directory);
+			}
 
-	while (!GetDirectories.eof())
-	{
-		string Directory;
-		getline(GetDirectories, Directory);
-		Directories.push_back(Directory);
-	}
+			GetDirectories.close();
 
-	GetDirectories.close();
+			ifstream GetFiles;
+			GetFiles.open("cleanup\\files.txt");
 
-	ifstream GetFiles;
-	GetFiles.open("cleanup\\files.txt");
+			while (!GetFiles.eof())
+			{
+				string File;
+				getline(GetFiles, File);
+				Files.push_back(File);
+			}
 
-	while (!GetFiles.eof())
-	{
-		string File;
-		getline(GetFiles, File);
-		Files.push_back(File);
-	}
+			GetFiles.close();
 
-	GetFiles.close();
+			ifstream GetAttachments;
+			GetAttachments.open("cleanup\\attachments.txt");
 
-	ifstream GetAttachments;
-	GetAttachments.open("cleanup\\attachments.txt");
+			while (!GetAttachments.eof())
+			{
+				string Attachment;
+				getline(GetAttachments, Attachment);
+				Attachments.push_back(Attachment);
+			}
 
-	while (!GetAttachments.eof())
-	{
-		string Attachment;
-		getline(GetAttachments, Attachment);
-		Attachments.push_back(Attachment);
-	}
+			GetAttachments.close();
 
-	GetAttachments.close();
-
-	for (unsigned i = 0; i < Directories.size(); ++i)
-	{
-		Files.erase(remove(Files.begin(), Files.end(), Directories[i]), Files.end());
-	}
-	Files.erase(remove(Files.begin(), Files.end(), "delete_orphaned.cpp"), Files.end());
-	Files.erase(remove(Files.begin(), Files.end(), "delete_orphaned.exe"), Files.end());
+		for (unsigned i = 0; i < Directories.size(); ++i)
+		{
+			Files.erase(remove(Files.begin(), Files.end(), Directories[i]), Files.end());
+		}
+		Files.erase(remove(Files.begin(), Files.end(), "delete_orphaned.cpp"), Files.end());
+		Files.erase(remove(Files.begin(), Files.end(), "delete_orphaned.exe"), Files.end());
 
 	vector<string> NotOrphans;
+		ifstream ImpFiles;
+			for (unsigned i = 0; i < Files.size(); ++i)
+			{
+				string File;
 
-	ifstream ImpFiles;
+				ImpFiles.open(Files[i]);
+				while (!ImpFiles.eof())
+				{
+					string Str;
+					ImpFiles >> Str;
+					File += Str + ' ';
+				}
 
-	for (unsigned i = 0; i < Files.size(); ++i)
-	{
-		string File;
+				ImpFiles.close();
 
-		ImpFiles.open(Files[i]);
-		while (!ImpFiles.eof())
+				for (unsigned i; i < Attachments.size(); ++i)
+				{
+					if (File.find(Attachments[i]) != string::npos) {
+						NotOrphans.push_back(Attachments[i]);
+					}
+				}
+
+			}
+
+		for (unsigned i = 0; i < Attachments.size(); ++i)
 		{
-			string Str;
-			ImpFiles >> Str;
-			File += Str + ' ';
-		}
-
-		ImpFiles.close();
-
-		for (unsigned i; i < Attachments.size(); ++i)
-		{
-			if (File.find(Attachments[i]) != string::npos) {
-				NotOrphans.push_back(Attachments[i]);
+			if (find(NotOrphans.begin(), NotOrphans.end(), Attachments[i]) == NotOrphans.end()) {
+				string DeleteOrphaned = "del \"Prisegtukai\\" + Attachments[i] + "\"";
+				system(DeleteOrphaned.c_str());
 			}
 		}
-
-	}
-
-	for (unsigned i = 0; i < Attachments.size(); ++i)
-	{
-		if (find(NotOrphans.begin(), NotOrphans.end(), Attachments[i]) == NotOrphans.end()) {
-			string DeleteOrphaned = "del \"Prisegtukai\\" + Attachments[i] + "\"";
-			system(DeleteOrphaned.c_str());
-		}
-	}
 	
 	system("rmdir /s /q cleanup");
 }
